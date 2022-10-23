f9f028a8f1131d5eb2cac14448d7fb6a7b6ca3b1
Petros <petros.chantz@gmail.com>
2022-10-23 19:06:51 +0200

    feat: adds color filters

--- src/components/Store/Filters/Color.tsx
+++ src/components/Store/Filters/Color.tsx
@@ -1,15 +1,22 @@
 export const Color = () => {
+  const productColors: string[][] = [
+    ["bg-red-400", "focus:ring-red-400", "text-red-400"],
+    ["bg-sky-400", "focus:ring-sky-400", "text-sky-400"],
+    ["bg-amber-400", "focus:ring-amber-400", "text-amber-400"],
+    ["bg-green-400", "focus:ring-green-400", "text-green-400"],
+  ];
+
   return (
     <div className="flex flex-col gap-5">
       <p>color</p>
       <div className="flex flex-row gap-2">
-        {["blue", "yellow", "red"].map((color) => (
+        {productColors.map(([color, focus, text]) => (
           <input
             id={`color-${color}`}
             aria-describedby={`color-${color}-description`}
             name={`color-${color}`}
             type="checkbox"
-            className={`h-5 w-5 rounded-full border-${color}-300 text-${color}-700 focus:ring-slate-500`}
+            className={`h-5 w-5 rounded-full ${color} ${focus} ${text}`}
           />
         ))}
       </div>
--- src/components/Store/Products/Product.tsx
+++ src/components/Store/Products/Product.tsx
@@ -1,7 +1,35 @@
 export const Product = (props: any) => {
+  const productColors: string[] = [
+    "bg-gray-300",
+    "bg-slate-300",
+    "bg-zinc-300",
+    "bg-neutral-300",
+    "bg-stone-300",
+    "bg-red-300",
+    "bg-orange-300",
+    "bg-amber-300",
+    "bg-yellow-300",
+    "bg-lime-300",
+    "bg-green-300",
+    "bg-emerald-300",
+    "bg-teal-300",
+    "bg-cyan-300",
+    "bg-sky-300",
+    "bg-indigo-300",
+    "bg-violet-300",
+    "bg-purple-300",
+    "bg-fuchsia-300",
+    "bg-pink-300",
+    "bg-rose-300",
+  ];
+
   return (
     <div className="flex flex-col h-full gap-3">
-      <div className={`h-32 bg-slate-200 w-full"`}></div>
+      <div
+        className={`h-32 ${
+          productColors[Math.floor(Math.random() * productColors.length)]
+        } w-full"`}
+      ></div>
       <div className="flex h-20 flex-col">
         <div className="flex flex-row justify-between">
           <p>{props.products.name}</p>
